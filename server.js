93c5439875d45dea6bc1418bb9a21ca2a30b66ba
Gafo <gabriel>
2014-11-01 13:15:14 +0100

    DotJs not working

--- server.js
+++ server.js
@@ -4,40 +4,39 @@ var fs = require('fs');
 var express = require('express');
 var config = JSON.parse(fs.readFileSync('config.json'));
 
-var dots = require("dot").process({path: "./views"});
-      dots.mytemplate({foo:"hello world"});
+// var dots = require("doT").process({path: "./views"});
+      // dots.mytemplate({foo:"hello world"});
 
 var host = config.host;
 var port = config.port;
 
 var app = express();
-
-
-app.set('views', './views');
-app.set("view engine", "def");
-app.engine('def', dots);
-
-var users = {
-	'1': {
-		'name': 'test1 test1',
-		'link': 'http://google.com'
-	},
-	'2': {
-		'name': 'test2 test2',
-		'link': 'http://abv.bg'
-	}
-}
+app.set('views', __dirname + '/views');
+app.set('view engine', 'dot' );
+app.engine('dot', require('express-dot').__express );
+// app.use(express.static(__dirname + '/public'));
 
 
 
-app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-	//res.status(200).send('hop');
-	res.render('home', {
-		title: 'hello DotJS'
-	});	
+app.get('/home', function(req, res) {
+	res.render('home' ,{title: 'title test'});	
 });
+
+
+app.listen(port, host);
+console.log("Running at " + host + ':' + port);
+
+// var users = {
+// 	'1': {
+// 		'name': 'test1 test1',
+// 		'link': 'http://google.com'
+// 	},
+// 	'2': {
+// 		'name': 'test2 test2',
+// 		'link': 'http://abv.bg'
+// 	}
+// }
 
 // app.get('/user/:id', function(req, res) {
 // 	var user = users[req.params.id];	
@@ -48,7 +47,4 @@ app.get('/', function(req, res) {
 // 	}
 // });
 
-app.listen(port, host);
 
-
-console.log("Running at " + host + ':' + port);
